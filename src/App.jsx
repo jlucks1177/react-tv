0e0981825cdb10550988c6791072cb9561e46764
Joe <jlucks1177@gmail.com>
2025-09-03 21:32:49 -0500

    Wrote the necessary JSX in order for the app to properly render the information using component files/functions and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import { tvShows } from "./shows/data.js";
+import ShowSelection from "./shows/ShowSelection.jsx";
+import ShowDetails from "./shows/ShowDetails.jsx";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,30 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return (
+      <section>
+        <h2>Show Details</h2>
+        <p>Currently no show selected. Please select a show.</p>
+      </section>
+    );
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
